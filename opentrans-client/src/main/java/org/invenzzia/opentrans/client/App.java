5e01504c2d34352f6318a09b637afd88c5646d33
Tomasz Jędrzejewski <nightstorm@tlen.pl>
2012-04-02 20:01:15 +0200

    Initial commit for the new project structure.

---
+++ opentrans-client/src/main/java/org/invenzzia/opentrans/client/App.java
@@ -0,0 +1,13 @@
+package org.invenzzia.opentrans.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

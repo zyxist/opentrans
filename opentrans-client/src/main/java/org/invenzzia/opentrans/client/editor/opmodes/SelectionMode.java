28b17fb5376d3ca95e3427d863a5708d93804b8d
Tomasz Jędrzejewski <nightstorm@tlen.pl>
2012-06-24 20:55:59 +0200

    client: adding proper headers to DrawingMode and SelectionMode.

--- opentrans-client/src/main/java/org/invenzzia/opentrans/client/editor/opmodes/SelectionMode.java
+++ opentrans-client/src/main/java/org/invenzzia/opentrans/client/editor/opmodes/SelectionMode.java
@@ -1,14 +1,30 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * OpenTrans - public transport simulator
+ * Copyright (c) 2011-2012 Invenzzia Group
+ * 
+ * OpenTrans is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenTrans is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenTrans. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.invenzzia.opentrans.client.editor.opmodes;
 
 import org.invenzzia.opentrans.client.ui.netview.IOperationMode;
 
 /**
- * Description here.
+ * In the selection mode, we can select the parts of the infrastructure
+ * and move them around. Such actions, as delete work and these are the
+ * only available modifications of the network graph content.
+ * 
+ * Right-clicking allows selecting i.e. the bitmap for the segment.
  * 
  * @author Tomasz Jędrzejewski
  */

26fc770ac2c1a5bb29b85d0bb4e692d1d0f27009
Tomasz Jędrzejewski <nightstorm@tlen.pl>
2012-08-02 20:07:01 +0200

    visitons: world resizing fully implemented.

--- opentrans-visitons/src/main/java/org/invenzzia/opentrans/visitons/world/World.java
+++ opentrans-visitons/src/main/java/org/invenzzia/opentrans/visitons/world/World.java
@@ -18,6 +18,7 @@
 package org.invenzzia.opentrans.visitons.world;
 
 import com.google.common.base.Preconditions;
+import org.invenzzia.opentrans.visitons.exception.WorldException;
 import org.invenzzia.opentrans.visitons.infrastructure.Vertex;
 
 /**
@@ -131,12 +132,12 @@ public class World {
 	public World extendHorizontally(HorizontalDir where) {
 		Segment[][] newSegments = new Segment[dimX + 1][dimY];
 		if(where == HorizontalDir.LEFT) {
-			this.copySegments(newSegments, 1, 0);
+			this.copySegmentsExt(newSegments, 1, 0);
 			for(int i = 0; i < dimY; i++) {
 				newSegments[0][i] = new Segment().setPosition(0, i);
 			}
 		} else {
-			this.copySegments(newSegments, 0, 0);
+			this.copySegmentsExt(newSegments, 0, 0);
 			for(int i = 0; i < dimY; i++) {
 				newSegments[dimX][i] = new Segment().setPosition(dimX, i);
 			}
@@ -155,18 +156,94 @@ public class World {
 	public World extendVertically(VerticalDir where) {
 		Segment[][] newSegments = new Segment[dimX][dimY + 1];
 		if(where == VerticalDir.UP) {
-			this.copySegments(newSegments, 0, 1);
+			this.copySegmentsExt(newSegments, 0, 1);
 			for(int i = 0; i < dimX; i++) {
 				newSegments[i][0] = new Segment().setPosition(i, 0);
 			}
 		} else {
-			this.copySegments(newSegments, 0, 0);
+			this.copySegmentsExt(newSegments, 0, 0);
 			for(int i = 0; i < dimX; i++) {
 				newSegments[i][dimY] = new Segment().setPosition(i, dimY);
 			}
 		}
 		this.segments = newSegments;
 		this.dimY++;
+		return this;
+	}
+	
+	/**
+	 * Shrinks the world. The conditions for performing this operation are:
+	 * 
+	 * <ul>
+	 *  <li>the world does not have a horizontal size equal to 1.</li>
+	 *  <li>all segments from the reduced side are unused.</li>
+	 * </ul>
+	 * 
+	 * @param where Exact resizing direction.
+	 * @return Fluent interface.
+	 * @throws WorldException If the conditions are not met.
+	 */
+	public World shrinkHorizontally(HorizontalDir where) throws WorldException {
+		if(this.dimX < 2) {
+			throw new WorldException("The minimal horizontal world size has been reached.", this);
+		}
+		int y;
+		int offset;
+		if(HorizontalDir.LEFT == where) {
+			y = 0;
+			offset = 1;
+		} else {
+			y = this.dimY - 1;
+			offset = 0;
+		}
+		for(int i = 0; i < this.dimX; i++) {
+			if(this.segments[i][y].isUsed()) {
+				throw new WorldException("There are segments in use in the reduced side of the world.", this);
+			}
+		}
+		// OK, now actual resize
+		Segment[][] newSegments = new Segment[dimX - 1][dimY];
+		this.copySegmentsShr(newSegments, offset, 0, dimX - 1, dimY);
+		this.segments = newSegments;
+		this.dimX--;
+		return this;
+	}
+	
+	/**
+	 * Shrinks the world. The conditions for performing this operation are:
+	 * 
+	 * <ul>
+	 *  <li>the world does not have a vertical size equal to 1.</li>
+	 *  <li>all segments from the reduced side are unused.</li>
+	 * </ul>
+	 * 
+	 * @param where Exact resizing direction.
+	 * @return Fluent interface.
+	 * @throws WorldException If the conditions are not met.
+	 */
+	public World shrinkVertically(VerticalDir where) throws WorldException {
+		if(this.dimY < 2) {
+			throw new WorldException("The minimal horizontal world size has been reached.", this);
+		}
+		int x;
+		int offset;
+		if(VerticalDir.UP == where) {
+			x = 0;
+			offset = 1;
+		} else {
+			x = this.dimX - 1;
+			offset = 0;
+		}
+		for(int i = 0; i < this.dimY; i++) {
+			if(this.segments[x][i].isUsed()) {
+				throw new WorldException("There are segments in use in the reduced side of the world.", this);
+			}
+		}
+		// OK, now actual resize
+		Segment[][] newSegments = new Segment[dimX][dimY - 1];
+		this.copySegmentsShr(newSegments, 0, offset, dimX, dimY - 1);
+		this.segments = newSegments;
+		this.dimY--;
 		return this;
 	}
 
@@ -177,10 +254,29 @@ public class World {
 	 * @param deltaX Optional shift in the X axis
 	 * @param deltaY Optional shift in the Y axis
 	 */
-	protected void copySegments(Segment[][] newArray, int deltaX, int deltaY) {
+	protected void copySegmentsExt(Segment[][] newArray, int deltaX, int deltaY) {
 		for(int x = 0; x < this.dimX; x++) {
 			for(int y = 0; y < this.dimY; y++) {
-				newArray[x + deltaX][y + deltaY] = this.segments[x][y].setPosition(x + deltaX, y + deltaY);
+				int dx = x + deltaX;
+				int dy = y + deltaY;
+				newArray[dx][dy] = this.segments[x][y].setPosition(dx, dy);
+			}
+		}
+	}
+	
+	/**
+	 * A helper method used to copy the contents of the segment array to a new one.
+	 *
+	 * @param newArray The new array
+	 * @param deltaX Optional shift in the X axis
+	 * @param deltaY Optional shift in the Y axis
+	 */
+	protected void copySegmentsShr(Segment[][] newArray, int deltaX, int deltaY, int newX, int newY) {
+		for(int x = 0; x < newX; x++) {
+			for(int y = 0; y < newY; y++) {
+				int dx = x + deltaX;
+				int dy = y + deltaY;
+				newArray[x][y] = this.segments[dx][dy].setPosition(x, y);
 			}
 		}
 	}

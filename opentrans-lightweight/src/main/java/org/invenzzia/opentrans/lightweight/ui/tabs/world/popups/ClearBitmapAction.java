f6096c6cb1f4081e7c6c5db4c27ad2e42d279523
zyxist <nightstorm@tlen.pl>
2013-06-18 20:40:54 +0200

    Clearing bitmap implemented.

--- opentrans-lightweight/src/main/java/org/invenzzia/opentrans/lightweight/ui/tabs/world/popups/ClearBitmapAction.java
+++ opentrans-lightweight/src/main/java/org/invenzzia/opentrans/lightweight/ui/tabs/world/popups/ClearBitmapAction.java
@@ -17,9 +17,16 @@
 
 package org.invenzzia.opentrans.lightweight.ui.tabs.world.popups;
 
+import com.google.inject.Inject;
+import org.invenzzia.helium.exception.CommandExecutionException;
+import org.invenzzia.helium.history.History;
 import org.invenzzia.opentrans.lightweight.annotations.PopupAction;
+import org.invenzzia.opentrans.lightweight.ui.IDialogBuilder;
 import org.invenzzia.opentrans.lightweight.ui.tabs.world.IEditModeAPI;
 import org.invenzzia.opentrans.lightweight.ui.tabs.world.IPopupAction;
+import org.invenzzia.opentrans.visitons.editing.ICommand;
+import org.invenzzia.opentrans.visitons.editing.operations.ClearBitmapCmd;
+import org.invenzzia.opentrans.visitons.network.Segment;
 
 /**
  * Allows clearing the background bitmap.
@@ -28,8 +35,21 @@ import org.invenzzia.opentrans.lightweight.ui.tabs.world.IPopupAction;
  */
 @PopupAction(text = "Clear bitmap")
 public class ClearBitmapAction implements IPopupAction {
+	@Inject
+	private History<ICommand> history;
+	@Inject
+	private IDialogBuilder dialogBuilder;
 
 	@Override
 	public void execute(IEditModeAPI api, double x, double y) {
+		try {
+			this.history.execute(new ClearBitmapCmd(
+				(int) Math.floor(x / Segment.SIZE_D),
+				(int) Math.floor(y / Segment.SIZE_D)
+			));
+			api.setStatusMessage("Bitmap loaded.");
+		} catch(CommandExecutionException exception) {
+			this.dialogBuilder.showError("Error while clearing the bitmap", exception);
+		}
 	}
 }
